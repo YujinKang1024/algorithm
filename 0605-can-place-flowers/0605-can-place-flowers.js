3f80b99e51347f8d63dfdbc52dd6759e7f009a66
YujinKang <yujinkang1024@gmail.com>
2024-08-24 21:16:08 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function (flowerbed, n) {
+    if (flowerbed.length === 1 && n === 1) {
+        return flowerbed[0] === 0 ? true : flase;
+    }
+
+    let count = n;
+
+    for (let i = 0; i < flowerbed.length; i++) {
+        if (flowerbed[i] === 0) {
+            if (flowerbed[i + 1] === 0 && flowerbed[i - 1] === 0) {
+                flowerbed[i] = 1;
+                count--;
+            }
+
+            if (i === 0 && flowerbed[i + 1] === 0) {
+                flowerbed[i] = 1;
+                count--;
+            }
+
+            if (i === flowerbed.length - 1 && flowerbed[i - 1] === 0) {
+                flowerbed[i] = 1;
+                count--;
+            }
+        }
+    }
+
+    return count <= 0;
+};
