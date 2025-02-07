7670df7adf43888034beff0e27bcb5c71cdb78f9
YujinKang <yujinkang1024@gmail.com>
2025-02-07 11:48:34 +0900

    Time: 82 ms (5.72%), Space: 52.9 MB (14.06%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let elemCount = nums.length;
+    let targetElem = 2; 
+
+    for (let i = 2; i < nums.length; i++) {
+        if (nums[i] === nums[targetElem-1] && nums[i] === nums[targetElem-2]) {
+            continue;
+        } else {
+            nums[targetElem] = nums[i];
+            targetElem++;
+        }
+    }
+
+    return targetElem;
+};
